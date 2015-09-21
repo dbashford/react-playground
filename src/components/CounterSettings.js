c5ded8607f2200a307a39ec778f5ecb238af5bf6
david.bashford <dbashford@bericotechnologies.com>
2015-09-21 15:20:45 -0400

    handling errors better

--- src/components/CounterSettings.js
+++ src/components/CounterSettings.js
@@ -49,7 +49,7 @@ export default class CounterSettings extends Component {
     }
   }
 
-  handleValidation(field, test, errorMessage, event) {
+  handleValidation(field, test, errorMessage, clearField, event) {
     const val = event.target.value;
     const newState = this.state;
     newState.settings[field] = val;
@@ -61,7 +61,7 @@ export default class CounterSettings extends Component {
     } else if (this.state.errors[field]) {
       // is valid, but has existing message, remove message
       newState.errors = this.state.errors;
-      if (errorMessage === newState.errors[field]) {
+      if (clearField || errorMessage === newState.errors[field]) {
         delete newState.errors[field];
       }
     }
@@ -70,11 +70,11 @@ export default class CounterSettings extends Component {
   }
 
   render() {
-    const amountValidation = this.handleValidation.bind(this, 'amount', isNumber, 'Amount must be a number.');
-    const amountPresentValidation = this.handleValidation.bind(this, 'amount', isPresent, 'Amount must be provided.');
+    const amountValidation = this.handleValidation.bind(this, 'amount', isNumber, 'Amount must be a number.', true);
+    const amountPresentValidation = this.handleValidation.bind(this, 'amount', isPresent, 'Amount must be provided.', false);
 
-    const intervalValidation = this.handleValidation.bind(this, 'interval', isNumber, 'Interval must be a number.');
-    const intervalPresentValidation = this.handleValidation.bind(this, 'interval', isPresent, 'Interval Milliseconds must be provided.');
+    const intervalValidation = this.handleValidation.bind(this, 'interval', isNumber, 'Interval must be a number.', true);
+    const intervalPresentValidation = this.handleValidation.bind(this, 'interval', isPresent, 'Interval Milliseconds must be provided.', false);
 
     return (
       <form styleName="container" onSubmit={this.onSubmit.bind(this)}>
